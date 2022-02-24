2d6e3aa8b10577937349b369718a2a0cfe98b7b6
juanbolanos00 <carl-bo@hotmail.com>
2022-02-23 21:28:19 -0500

    Se muestra puntajes desde Firestore

--- app/src/main/java/com/example/memorama/ScoresView.java
+++ app/src/main/java/com/example/memorama/ScoresView.java
@@ -1,16 +1,36 @@
 package com.example.memorama;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ScoresView extends AppCompatActivity {
 
     Button back;
+    TableLayout table;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public static final String TAG = "MYDOC";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +38,97 @@ public class ScoresView extends AppCompatActivity {
         setContentView(R.layout.scores_view);
 
         back = findViewById(R.id.btnBack);
+        table = findViewById(R.id.scoresTable);
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Regresando a Inicio...");
                 goToStart();
+
             }
         });
+
+        db.collection("scores")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                System.out.println(document.getId() + " => " + document.getData());
+
+                                TableRow tr = new TableRow(ScoresView.this);
+                                tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
+                                // textwiew username
+                                TextView txtUsername = new TextView(ScoresView.this);
+                                txtUsername.setText(document.getData().get("user").toString());
+                                txtUsername.setLayoutParams(new TableRow.LayoutParams( TableRow.LayoutParams.WRAP_CONTENT));
+                                txtUsername.setTextSize(TypedValue.COMPLEX_UNIT_SP,16);
+                                txtUsername.setPadding(25,0,0,0);
+                                tr.addView(txtUsername);
+                                // textview scores
+                                TextView txtScores = new TextView(ScoresView.this);
+                                txtScores.setText(document.getData().get("score").toString());
+                                txtScores.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
+                                txtScores.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                                txtScores.setTextSize(TypedValue.COMPLEX_UNIT_SP,16);
+                                tr.addView(txtScores);
+                                table.addView(tr);
+                            }
+//                            System.out.println("size1 "+scoresMap.size());
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+
+
+
+                    }
+
+                });
+
+
+
     }
 
     private void goToStart() {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
+
+    private HashMap<String, Integer> getAllScores() {
+        HashMap<String, Integer> scoresMap = new HashMap<String, Integer>();
+
+        db.collection("scores")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                System.out.println(document.getId() + " => " + document.getData());
+
+                                scoresMap.put(document.getData().get("user").toString(),
+                                        Integer.parseInt(document.getData().get("score").toString()));
+
+                            }
+                            System.out.println("size1 "+scoresMap.size());
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        System.out.println("size 2 "+scoresMap.size());
+
+                    }
+
+                });
+        System.out.println("size 3 "+scoresMap.size());
+        for (String i : scoresMap.keySet()) {
+            System.out.println("key: " + i + " value: " + scoresMap.get(i));
+        }
+
+        return scoresMap;
+    }
+
 }
